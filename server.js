14f310865ed746d43caf1d5b81f61867a4febf72
Ashwani <vcsashwani@gmail.com>
2023-12-06 20:27:41 +0530

    mern setup

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const colors = require("colors");
+const morgan = require("morgan");
+const cors = require("cors");
+const connectDB = require("./config/db");
+//dot config
+dotenv.config();
+
+//mongodb connection
+connectDB();
+
+//rest object
+const app = express();
+
+//middlewares
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+
+//routes
+app.get('/', (req, res) => {
+    res.send('Hello, this is your Node.js server!');
+  });
+
+app.use("/api/v1/test", require("./routes/testRoutes"));
+
+//port
+const PORT = process.env.PORT || 8080;
+
+//listen
+app.listen(PORT, () => {
+  console.log(
+    `Node Server Running In ${process.env.DEV_MODE} ModeOn Port ${process.env.PORT}`
+      .bgBlue.white
+  );
+});
